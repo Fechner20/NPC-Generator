fff3d2144d10c1c325defe4fa9547d3d5b5afbc0
James Fechner <james.d.fechner@gmail.com>
2025-10-27 17:26:13 +0000

    first day

---
+++ generator.py
@@ -0,0 +1,10 @@
+names = input("Input 10 Names seperated by spaces: ")
+listofnames= names.split
+import random
+Heights=["5'4", "6'6", "5'7", "6'", "5'10"]
+Weapons=["Broadsword", "Dagger", "Musket", "Bayyonette", "Bow and Arrow", "Throwing Knives"]
+Gender=["Male", "Female"]
+Size= 
+
+
+
