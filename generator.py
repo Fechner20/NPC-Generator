2524b225519a39d4f06c37352bb13a82c7ce3d88
James Fechner <james.d.fechner@gmail.com>
2025-10-28 18:24:45 +0000

    generator day 2

--- generator.py
+++ generator.py
@@ -1,10 +1,36 @@
-names = input("Input 10 Names seperated by spaces: ")
-listofnames= names.split
+mannames = ["James", "Jaden", "Brandon", "Logan", "Dexter", "Sylvester", "Edward"]
+femalenames = ["Ashley", "Sarah","Emily","Jamie","Bertha","Ally", "Dorothy"]
+amountofNPC= input("How many NPCS do you want to generate(Must be 10+): ")
 import random
+
+#Possible Attributes for NPCS
 Heights=["5'4", "6'6", "5'7", "6'", "5'10"]
-Weapons=["Broadsword", "Dagger", "Musket", "Bayyonette", "Bow and Arrow", "Throwing Knives"]
+Weapons=["Broadsword", "Dagger", "Musket", "Bayyonette", "Bow and Arrow", "Throwing Knife"]
 Gender=["Male", "Female"]
-Size= 
+Age = random.int(1,100)
+
+
+gender=random.choice(Gender)
+
+# GENERATOR
+
+for a in range(amountofNPC):
+ if gender == "Female":
+    print(f"Name: {random.choice(femalenames)}")
+else:
+    print(f"Name: {random.choice(mannames)}")
+print(f"Height: {random.choice(Heights)}")
+print(f"Weapon: {random.choice(Weapons)}")
+print(f"Gender: {random.choice(Gender)}")
+print(f"Age: {(Age)}")
+
+
+
+
+   
+    
+
+
 
 
 
