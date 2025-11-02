112189b39053d457c98c1e166f030e15f707752d
James Fechner <james.d.fechner@gmail.com>
2025-11-02 18:10:32 +0000

    new and improved generator

---
+++ generator2.py
@@ -0,0 +1,11 @@
+amountofNPC= int(input("How many NPCS do you want to generate(Must be 10+): ")) #USER INPUT ON HOW MANY NPCS TO MAKE
+import random
+
+#Possible Attributes for NPCS
+Heights=[5.4, 5, 5.7, 6, 6.3,]
+Weapons=["Broadsword", "Dagger", "Musket", "Bayyonette", "Bow and Arrow", "Throwing Knife"]
+Jobs=["Blacksmith", "Cooper", "Servant","Knight","Soldier", "King"]
+Names = ["James", "Jaden", "Brandon", "Logan", "Dexter", "Sylvester", "Edward", "Ashley", "Sarah","Emily","Jamie","Bertha","Ally", "Dorothy"]
+#FINAL F STRING TO PRINT ALL THE NPCS
+for i in range(amountofNPC):
+    print(f"\nName:{random.choice(Names)} \nWeapon: {random.choice(Weapons)} \nJob: {random.choice(Jobs)} \nHeight: {random.choice(Heights)} feet tall \nAge: {random.randint(1,100)}")
